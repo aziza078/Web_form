2c74b773065b1885d9e6d1daf0560e7051e5b043
aziza078 <cafarova.2007@gmail.com>
2025-11-06 11:01:29 +0400

    Add files via upload

---
+++ quiz_result.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <title>Quiz Result</title>
+</head>
+<body>
+  <h1>Quiz Submission Result</h1>
+  <pre>
+<?php
+print_r($_POST);
+?>
+  </pre>
+</body>
+</html>
